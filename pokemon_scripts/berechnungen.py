4f7583def0c47c786d3cfa8348c530588cb9c3dd
Mecke Dev <crafted.mecke@gmail.com>
2020-02-09 20:40:12 +0100

    Background-Scripts to get the RPG-Part working

---
+++ pokemon_scripts/berechnungen.py
@@ -0,0 +1,85 @@
+import json
+import numpy
+import random
+
+def fight_xp(self, target):
+
+    trainer = True
+
+    if trainer:
+        a = 1.5
+    else:
+        a = 1.0
+
+    b = self.basis_xp
+
+    if self.getauscht:
+        t = 1.5
+    else:
+        t = 1
+
+    if "Glücks-Ei" in self.items:
+        g = 1.5
+    else:
+        g = 1
+
+    L = target.level
+
+    # print(f"{a}*{b}*{t}*{g}*{L})//7")
+    xp = (a*b*t*g*L)//7
+
+    print(xp)
+
+def spawn_xp(level, xp_typ):
+
+    # XP-Typ 1
+    if xp_typ == 1:
+        return (level ** 3) * 1.25
+
+    # XP-Typ 2
+    elif xp_typ == 2:
+        return level ** 3
+
+    # XP-Typ 3
+    elif xp_typ == 3:
+        return (1.2 * (level ** 3)) - (15 * (level ** 2)) + (100 * level - 140)
+
+    # XP-Typ 4
+    elif xp_typ == 4:
+        return (level**3) * 0.8
+
+def xp_typen(entwicklungen):
+
+    with open('XP-Typen.json', 'r', encoding='utf-8') as xp:
+        xp_typ = json.load(xp)
+
+        xp1 = xp_typ["1"]
+        xp2 = xp_typ["2"]
+        xp3 = xp_typ["3"]
+        xp4 = xp_typ["4"]
+
+        for elem in entwicklungen:
+
+            if elem in xp1:
+                return 1
+
+            if elem in xp2:
+                return 2
+
+            if elem in xp3:
+                return 3
+
+            if elem in xp4:
+                return 4
+
+def geschlecht(self):
+
+    genders     =  self.geschlechter
+
+    rand_val = random.random()
+    total = 0
+    for k, v in genders.items():
+        total += v
+        if rand_val <= total/100:
+            print(rand_val)
+            return k
---
+++ pokemon_scripts/poke.py
@@ -0,0 +1,98 @@
+import os
+import json
+from berechnungen import *
+
+def cls():
+   if os.name=='nt':
+        os.system('cls')
+   else:
+        os.system('clear')
+
+class Pokemon:
+
+    def __init__(self, num, level):
+
+        with open('pokemon.json', 'r', encoding='utf-8') as p:
+            all_pokemon = json.load(p)
+
+        poke = all_pokemon[f"{num}"][0]
+
+        self.nummer             = poke["Nummer"]
+        self.name               = poke["Name"]
+        self.typ                = poke["Typ"]
+        self.schwaechen         = poke["Schwächen"]
+
+        # Pokemon Inventar
+        self.items              = []
+
+        self.beschreibung       = poke["Beschreibung"]
+        self.getauscht          = False
+
+        self.entwicklungen      = poke["Entwicklungen"]
+        self.needs              = poke["Needs"]
+
+        self.faehigkeiten       = poke["Fähigkeiten"]
+        self.geschlechter       = poke["Geschlechter"]
+        self.geschlecht         = geschlecht(self)
+        self.basiswerte         = poke["Basiswerte"]
+        self.basis_xp           = poke["Basis-XP"]
+        self.xp_typ             = xp_typen(self.entwicklungen)
+        self.xp                 = spawn_xp(level, self.xp_typ)
+        self.fundorte           = poke["Fundorte"]
+        self.attacken           = poke["Attacken"]
+        self.level              = level
+
+        self.tms                = poke["TMs"]
+        self.tps                = poke["TPs"]
+        self.fangrate           = poke["Fangrate"]
+        self.ei_zyklen          = poke["Ei-Zyklen"]
+
+
+    def get_stats(self):
+
+        print(f"Name\t\t: {self.name}")
+        print(f"Nummer\t\t: #{self.nummer}")
+        print(f"XP\t\t: {self.xp}")
+        print(f"Level\t\t: {self.level}")
+
+        y = "Typ\t\t: "
+        i = 0
+        while i < len(self.typ):
+            y += f"{self.typ[i]}, "
+            i += 1
+        y = y[:-2]
+        print(y)
+
+        print(f"XP-Typ\t\t: {self.xp_typ}")
+        print(f"Geschlecht\t: {self.geschlecht}")
+        
+        print()        
+        print("Stats: ")
+        print()
+        
+        i = 0
+        for w in self.basiswerte:
+            print(f"{tuple(self.basiswerte.items())[i][0]}".ljust(25, ' ') + ":" + f"{tuple(self.basiswerte.items())[i][1]}".ljust(10, ' '))
+            i += 1
+
+cls()
+
+with open('pokemon.json', 'r', encoding='utf-8') as pokemons:
+    pokes = json.load(pokemons)
+
+
+while True:
+
+    dex = input("Bitte gib die Pokedex-Nummer ein: \n0 für Ende\n")
+    dex = dex.split(' ')
+
+    cls()
+    if dex == 0:
+        break
+
+    elif int(dex[0]) <= len(pokes):
+        test = Pokemon(int(dex[0]), int(dex[1]))
+        test.get_stats()
+
+    else:
+        print("Pokemon noch nicht im Pokedex!")
