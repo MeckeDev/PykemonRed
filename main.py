432ac9294aa2fb147864bac77d9a31455b0026cd
Mecke Dev <crafted.mecke@gmail.com>
2020-02-10 20:36:18 +0100

    NPC Trainer
    Trainer hinzugefügt und Hitboxen mit Spieler-Detection

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ from character import *
 pygame.init()
 
 # Die Auflösung des Fensters wird festgelegt
-screenwidth = 144*3                             # Breite des Fensters
-screenheight = 160*3                            # Höhe des Fensters
+screenheight = 576                           # Höhe des Fensters
+screenwidth = 640                            # Breite des Fensters
 
 # Das Fenster wird mit den angegeben Maßen erstellt
-win = pygame.display.set_mode((screenheight, screenwidth))
+win = pygame.display.set_mode((screenwidth, screenheight))
 
 # Der Titel des Fensters wird festgelegt
 pygame.display.set_caption("Pykemon Rot by Mecke_Dev")
 
 # Der Character des Spielers wird erstellt
-player = Character(20, 20, 32, 32)
+player = Character(50, 50, 32, 32, "player")
 
 # Test-Gegner generieren
-trainer = Character(80, 80, 32, 32)
-trainer.lastpos = "r"
+t1 = Character(150, 150, 32, 32, "t1", True, True)
 
 # Eine Uhr wird erstellt um die FPS für die Animationen festzulegen
 clock = pygame.time.Clock()
@@ -29,21 +28,25 @@ clock = pygame.time.Clock()
 def redrawGameWindow():
     win.fill((0, 0, 0))                         # Füllt das Fenster mit Schwarzer Farbe
     player.draw(win)                            # Zeichnet den Character an seiner aktuellen Position
-    trainer.draw(win)                            # Zeichnet den Test-Gegner an seiner aktuellen Position
+    t1.draw(win)                            # Zeichnet den Test-Gegner an seiner aktuellen Position
     pygame.display.update()                     # Zeigt uns das neu "gezeichnete" Bild an
 
+
+trainers = [t1]
 
 # Die Endlos-Schleife die das Spiel am laufen hält, bis wir es beenden
 run = True
 while run:
 
     # Hier legen wir die FPS fest, also wie oft das Bild in unserem Fenster aktualisiert werden soll
-    clock.tick(24)
+    clock.tick(16)
 
     # Funktion um das Spiel zu beenden wenn der Nutzer den Schließen-Knopf benutzt
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    t1.is_collided_with(player)
 
     # keys beinhaltet alle möglichen Tasten die man auf der Tastatur drücken kann
     keys = pygame.key.get_pressed()
@@ -51,32 +54,32 @@ while run:
     # Hier lernt unser Character laufen
 
     # Wenn der Spieler A oder Pfeil nach Links drückt, bewegt der Character sich nach Links
-    if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and player.x > player.vel:
-        player.x -= player.vel                  # versetzt die Position des Characters nach links
+    if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and player.y > player.vel:
+        player.y -= player.vel                  # versetzt die Position des Characters nach links
         player.left = True                      # aktiviert die Bewegung nach links    <---
         player.right = False                    # deaktiviert die Bewegung nach rechts
         player.up = False                       # deaktiviert die Bewegung nach oben
         player.down = False                     # deaktiviert die Bewegung nach unten
 
     # Wenn der Spieler D oder Pfeil nach Rechts drückt, bewegt der Character sich nach Links
-    elif (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and player.x < screenwidth - player.width:
-        player.x += player.vel                  # versetzt die Position des Characters nach rechts
+    elif (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and player.y + player.width < screenwidth:
+        player.y += player.vel                  # versetzt die Position des Characters nach rechts
         player.right = True                     # aktiviert die Bewegung nach rechts      <---
         player.left = False                     # deaktiviert die Bewegung nach links
         player.up = False                       # deaktiviert die Bewegung nach oben
         player.down = False                     # deaktiviert die Bewegung nach unten
 
     # Wenn der Spieler W oder Pfeil nach Oben drückt, bewegt der Character sich nach Oben
-    elif (keys[pygame.K_w] or keys[pygame.K_UP]) and player.y > player.vel:
-        player.y -= player.vel                  # versetzt die Position des Characters nach oben
+    elif (keys[pygame.K_w] or keys[pygame.K_UP]) and player.x - player.vel > 0:
+        player.x -= player.vel                  # versetzt die Position des Characters nach oben
         player.left = False                     # deaktiviert die Bewegung nach links
         player.right = False                    # deaktiviert die Bewegung nach rechts
         player.up = True                        # aktiviert die Bewegung nach oben        <---
         player.down = False                     # deaktiviert die Bewegung nach unten
 
     # Wenn der Spieler S oder Pfeil nach Unten drückt, bewegt der Character sich nach Unten
-    elif (keys[pygame.K_s] or keys[pygame.K_DOWN]) and player.y < screenheight - player.height - player.vel:
-        player.y += player.vel                  # versetzt die Position des Characters nach unten
+    elif (keys[pygame.K_s] or keys[pygame.K_DOWN]) and player.x < screenheight - player.height:
+        player.x += player.vel                  # versetzt die Position des Characters nach unten
         player.left = False                     # deaktiviert die Bewegung nach links
         player.right = False                    # deaktiviert die Bewegung nach rechts
         player.up = False                       # deaktiviert die Bewegung nach oben
--- pokemon_scripts/character.py
+++ pokemon_scripts/character.py
@@ -1,38 +1,26 @@
 import pygame
 
-# Arrays mit Einzelbildern für die Animationen
-
-# Animation beim nach unten laufen
-walkDown = [pygame.image.load('pic/player/playerF1.png'), pygame.image.load('pic/player/playerF2.png'),
-            pygame.image.load('pic/player/playerF3.png'), pygame.image.load('pic/player/playerF4.png')]
-
-# Animation beim nach oben laufen
-walkUp = [pygame.image.load('pic/player/playerB1.png'), pygame.image.load('pic/player/playerB2.png'),
-          pygame.image.load('pic/player/playerB3.png'), pygame.image.load('pic/player/playerB4.png')]
-
-# Animation beim nach rechts laufen
-walkRight = [pygame.image.load('pic/player/playerR1.png'), pygame.image.load('pic/player/playerR2.png'),
-             pygame.image.load('pic/player/playerR3.png'), pygame.image.load('pic/player/playerR4.png')]
-
-# Animation beim nach links laufen
-walkLeft = [pygame.image.load('pic/player/playerL1.png'), pygame.image.load('pic/player/playerL2.png'),
-            pygame.image.load('pic/player/playerL3.png'), pygame.image.load('pic/player/playerL4.png')]
-
 
 # Die Klasse Character wird erstellt
 class Character:
 
     # Initialisierung eines neuen Characters
-    def __init__(self, x, y, width, height):
+    def __init__(self, y, x, width, height, sprite, walk=False, is_trainer= False):
+
+        self.i = 1
 
         # Character Position x und y (0, 0) ist Oben Links
-        self.x = x                  # Position auf der x - Achse // Oben - Unten
-        self.y = y                  # Position auf der y - Achse // Links - Rechts
+        self.y = y                  # Position auf der x - Achse // Oben - Unten
+        self.x = x                  # Position auf der y - Achse // Links - Rechts
 
         # Character Größe und Geschwindigkeit
         self.width = width          # Breite des Characters
         self.height = height        # Höhe des Characters
         self.vel = 5                # Wie schnell bewegt sich der Character
+        self.sprite = sprite        # Aussehen des Characters
+
+        # Hitbox for Character
+        self.hitbox = (self.x, self.y, 32, 32)
 
         # Character Bewegungen // zu beginn bewegen wir uns in keine Richtung
         self.right = False          # Bewegung nach Rechts
@@ -40,7 +28,7 @@ class Character:
         self.up = False             # Bewegung nach Oben
         self.down = False           # Bewegung nach Unten
         self.walkCount = 0          # Anzahl der Schritte werden gezählt, für den Ablauf der Animation
-        self.lastpos = "d"          # Lässt uns wissen in welche Richtung der Character zuletzt geschaut hat
+        self.lastpos = "l"          # Lässt uns wissen in welche Richtung der Character zuletzt geschaut hat
 
         self.pokemon = []           # Pokemon die der Spieler bei sich trägt
 
@@ -56,47 +44,183 @@ class Character:
         self.pokebox9 = []
         self.pokebox10 = []
 
+        # Variablen die für Trainer benötigt werden die sich bewegen
+
+        self.walk = walk
+        self.end = 250
+        self.path = [self.x, self.end]
+        self.trainer = is_trainer
+
         # Flags um zu überprüfen ob Spieler bestimmte Events im Spiel getriggert hat
         self.flags = []
+
+        # Bilder für Character und ANimation festlegen
+        self.walkDown = []
+        self.walkUp = []
+        self.walkRight = []
+        self.walkLeft = []
+
+        self.set_sprite(sprite)
+        self.img = self.walkDown[0]
+        self.rect = self.img.get_rect()
 
     # Funktion um den Character zum laufen zu bringen // Animation
     def draw(self, win):
 
-        # Wenn der Schrittzähler > als 12 ist beginnt die Animation erneut
-        if self.walkCount + 1 >= 12:
-            self.walkCount = 0
+        # Funktion um bewegliche NPC laufen zu lassen
+        if self.walk:
+            self.move()
+            if self.walkCount + 1 > 12:
+                self.walkCount = 0
 
-        # Bewegt den Character nach Links und spielt die Animation ab
-        if self.left:
-            win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))       # Durchläuft die Animation - nach Links
-            self.lastpos = "l"                                              # setzt die letzte Blickrichtung auf Links
-            self.walkCount += 1                                             # Erhöht die Schrittzahl für die Animation
+            if self.vel > 0:
+                win.blit(self.walkRight[self.walkCount // 3], (self.y, self.x))  # Durchläuft die Animation - nach Links
+                self.lastpos = "r"                                               # setzt die letzte Blickrichtung auf Links
+                self.walkCount += 1                                              # Erhöht die Schrittzahl für die Animation
 
-        # Bewegt den Character nach Rechts und spielt die Animation ab
-        elif self.right:
-            win.blit(walkRight[self.walkCount // 3], (self.x, self.y))      # Durchläuft die Animation - nach Rechts
-            self.lastpos = "r"                                              # setzt die letzte Blickrichtung auf Rechts
-            self.walkCount += 1                                             # Erhöht die Schrittzahl für die Animation
+            else:
+                win.blit(self.walkLeft[self.walkCount // 3], (self.y, self.x))   # Durchläuft die Animation - nach Links
+                self.lastpos = "l"                                               # setzt die letzte Blickrichtung auf Links
+                self.walkCount += 1                                              # Erhöht die Schrittzahl für die Animation
 
-        # Bewegt den Character nach Oben und spielt die Animation ab
-        elif self.up:
-            win.blit(walkUp[self.walkCount // 3], (self.x, self.y))         # Durchläuft die Animation - nach Oben
-            self.lastpos = "u"                                              # setzt die letzte Blickrichtung auf Oben
-            self.walkCount += 1                                             # Erhöht die Schrittzahl für die Animation
+            if self.trainer:
+                if self.lastpos == "d":
+                    self.hitbox = (self.y, self.x+32, 32, 160)
+                if self.lastpos == "r":
+                    self.hitbox = (self.y+32, self.x, 160, 32)
+                if self.lastpos == "l":
+                    self.hitbox = (self.y - 192, self.x, 192, 32)
+                if self.lastpos == "u":
+                    self.hitbox = (self.y, self.x-192, 32, 192)
+            else:
+                self.hitbox = (self.y, self.x, 32, 32)
 
-        # Bewegt den Character nach Unten und spielt die Animation ab
-        elif self.down:
-            win.blit(walkDown[self.walkCount // 3], (self.x, self.y))       # Durchläuft die Animation - nach Unten
-            self.lastpos = "d"                                              # setzt die letzte Blickrichtung auf Unten
-            self.walkCount += 1                                             # Erhöht die Schrittzahl für die Animation
+            pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
 
-        # Wenn der Charcter aufhört sich zu bewegen bleibt er stehen und blickt in die letzte Laufrichtung
+        #                                                                            ###    Spieler bewegt sich    ###
         else:
-            if self.lastpos == "d":
-                win.blit(walkDown[0], (self.x, self.y))                     # lässt den Chacter nach unten schauen
-            if self.lastpos == "u":
-                win.blit(walkUp[0], (self.x, self.y))                       # lässt den Chacter nach oben schauen
-            if self.lastpos == "r":
-                win.blit(walkRight[0], (self.x, self.y))                    # lässt den Chacter nach rechts schauen
-            if self.lastpos == "l":
-                win.blit(walkLeft[0], (self.x, self.y))                     # lässt den Chacter nach links schauen
+            # Wenn der Schrittzähler > als 12 ist beginnt die Animation erneut
+            if self.walkCount + 1 >= 12:
+                self.walkCount = 0
+
+            # Bewegt den Character nach Links und spielt die Animation ab
+            if self.left:
+                win.blit(self.walkLeft[self.walkCount // 3], (self.y, self.x))   # Durchläuft die Animation - nach Links
+                self.lastpos = "l"                                               # setzt die letzte Blickrichtung auf Links
+                self.walkCount += 1                                              # Erhöht die Schrittzahl für die Animation
+
+            # Bewegt den Character nach Rechts und spielt die Animation ab
+            elif self.right:
+                win.blit(self.walkRight[self.walkCount // 3], (self.y, self.x))  # Durchläuft die Animation - nach Rechts
+                self.lastpos = "r"                                               # setzt die letzte Blickrichtung auf Rechts
+                self.walkCount += 1                                              # Erhöht die Schrittzahl für die Animation
+
+            # Bewegt den Character nach Oben und spielt die Animation ab
+            elif self.up:
+                win.blit(self.walkUp[self.walkCount // 3], (self.y, self.x))     # Durchläuft die Animation - nach Oben
+                self.lastpos = "u"                                               # setzt die letzte Blickrichtung auf Oben
+                self.walkCount += 1                                              # Erhöht die Schrittzahl für die Animation
+
+            # Bewegt den Character nach Unten und spielt die Animation ab
+            elif self.down:
+                win.blit(self.walkDown[self.walkCount // 3], (self.y, self.x))   # Durchläuft die Animation - nach Unten
+                self.lastpos = "d"                                               # setzt die letzte Blickrichtung auf Unten
+                self.walkCount += 1                                              # Erhöht die Schrittzahl für die Animation
+
+            # Wenn der Charcter aufhört sich zu bewegen bleibt er stehen und blickt in die letzte Laufrichtung
+            else:
+                if self.lastpos == "d":
+                    win.blit(self.walkDown[0], (self.y, self.x))                 # lässt den Chacter nach unten schauen
+                if self.lastpos == "u":
+                    win.blit(self.walkUp[0], (self.y, self.x))                   # lässt den Chacter nach oben schauen
+                if self.lastpos == "r":
+                    win.blit(self.walkRight[0], (self.y, self.x))                # lässt den Chacter nach rechts schauen
+                if self.lastpos == "l":
+                    win.blit(self.walkLeft[0], (self.y, self.x))                 # lässt den Chacter nach links schauen
+            self.hitbox = (self.y, self.x, 32, 32)
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+
+    def is_collided_with(self, p):
+        # if p.hitbox[1] < self.hitbox[1] + self.hitbox[3] and p.hitbox[1] + p.hitbox[3] > self.hitbox[1]:        # Checkt Hitboxen Oben und Unten
+        #    print("Battle")
+        if self.lastpos == "r":
+            if self.hitbox[1] < p.hitbox[1] + p.hitbox[3] and self.hitbox[1] + self.hitbox[3] > p.hitbox[1]:
+                if self.hitbox[0] + self.hitbox[2] > p.hitbox[0] and self.hitbox[0] < p.hitbox[0] + p.hitbox[2]:
+                    print(f"Kampf gefunden: \t{self.i}")
+                    self.i += 1
+        if self.lastpos == "l":
+            if self.hitbox[1] < p.hitbox[1] + p.hitbox[3] and self.hitbox[1] + self.hitbox[3] > p.hitbox[1]:
+                if self.hitbox[0] + self.hitbox[2] > p.hitbox[0] and self.hitbox[0] < p.hitbox[0] + p.hitbox[2]:
+                    print(f"Kampf gefunden: \t{self.i}")
+                    self.i += 1
+
+    def set_sprite(self, sprite):
+        # Arrays mit Einzelbildern für die Animationen für jeden Character
+
+        #    Player   #                                                                 #   Player  #
+
+        if sprite == "player":
+            # Animation beim nach unten laufen
+            self.walkDown = [pygame.image.load('pic/player/playerF1.png'),
+                             pygame.image.load('pic/player/playerF2.png'),
+                             pygame.image.load('pic/player/playerF3.png'),
+                             pygame.image.load('pic/player/playerF4.png')]
+
+            # Animation beim nach oben laufen
+            self.walkUp = [pygame.image.load('pic/player/playerB1.png'),
+                           pygame.image.load('pic/player/playerB2.png'),
+                           pygame.image.load('pic/player/playerB3.png'),
+                           pygame.image.load('pic/player/playerB4.png')]
+
+            # Animation beim nach rechts laufen
+            self.walkRight = [pygame.image.load('pic/player/playerR1.png'),
+                              pygame.image.load('pic/player/playerR2.png'),
+                              pygame.image.load('pic/player/playerR3.png'),
+                              pygame.image.load('pic/player/playerR4.png')]
+
+            # Animation beim nach links laufen
+            self.walkLeft = [pygame.image.load('pic/player/playerL1.png'),
+                             pygame.image.load('pic/player/playerL2.png'),
+                             pygame.image.load('pic/player/playerL3.png'),
+                             pygame.image.load('pic/player/playerL4.png')]
+
+        #   Trainer 1   #                                                               #   Trainer 1   #
+
+        if sprite == "t1":
+            # Animation beim nach unten laufen
+            self.walkDown = [pygame.image.load('pic/player/playerF1.png'),
+                             pygame.image.load('pic/player/playerF2.png'),
+                             pygame.image.load('pic/player/playerF3.png'),
+                             pygame.image.load('pic/player/playerF4.png')]
+
+            # Animation beim nach oben laufen
+            self.walkUp = [pygame.image.load('pic/player/playerB1.png'),
+                           pygame.image.load('pic/player/playerB2.png'),
+                           pygame.image.load('pic/player/playerB3.png'),
+                           pygame.image.load('pic/player/playerB4.png')]
+
+            # Animation beim nach rechts laufen
+            self.walkRight = [pygame.image.load('pic/player/playerR1.png'),
+                              pygame.image.load('pic/player/playerR2.png'),
+                              pygame.image.load('pic/player/playerR3.png'),
+                              pygame.image.load('pic/player/playerR4.png')]
+
+            # Animation beim nach links laufen
+            self.walkLeft = [pygame.image.load('pic/player/playerL1.png'),
+                             pygame.image.load('pic/player/playerL2.png'),
+                             pygame.image.load('pic/player/playerL3.png'),
+                             pygame.image.load('pic/player/playerL4.png')]
+
+    def move(self):
+        if self.vel > 0:
+            if self.y + self.vel < self.path[1]:
+                self.y += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
+        else:
+            if self.y - self.vel > self.path[0]:
+                self.y += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
